94cfa40992c3a759e449912d03b6a86558874b8b
Shawn Fladager <shawnfladager@gmail.com>
2023-09-11 11:46:55 +0800

    Initial layout for each component.

--- components/blockchain/PoolDetails.tsx
+++ components/blockchain/PoolDetails.tsx
@@ -1,10 +1,97 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import MaxEthChart from "../graphs/MaxEthChart"
 
+import { shortenAddress } from '@lib/utils/shortenAddress'
+
+// logos
+import balancer from '@/public/logos/balancer-logo.png'
+import curve from '@/public/logos/crv-logo.png'
 
 const PoolDetails = () => {
 
+  const maxEthData = [
+    { name: 'TryLSD', value: 400 },
+    { name: 'Balancer', value: 400 },
+  ]
+
+  const curvePoolData = [
+    { name: 'wstETH', value: 33.17 },
+    { name: 'rETH', value: 33.45 },
+    { name: 'sfrxETH', value: 33.38 },
+  ]
+  const balancerPoolData = [
+    { name: 'wstETH', value: 23.03 },
+    { name: 'rETH', value: 13.07 },
+    { name: 'sfrxETH', value: 63.89 },
+  ]
+
   return (
-    <div>
-      <h1>Pool Details</h1>
+    <div className='flex flex-col border-2 border-border rounded-xl m-3 p-4 md:w-full lg:full'>
+      <h2 className='text-secondary text-xl'>MaxETH Details</h2>
+      <div className='flex gap-3 items-center my-3'>
+        <div className='shrink-0'>
+          <MaxEthChart data={maxEthData} />
+        </div>
+        {/* ************ MaxETH pool details ***********/}
+        <div className='w-full'>
+          <div>
+            <div className='flex items-center justify-between'>
+              <div className='flex items-center'>
+                <Image alt='balancer' src={balancer} className='inline-block align-middle mr-1' height={20} width={20} />
+                <p>Balancer</p>
+              </div>
+              <p>512.21 ETH <span className='text-red'>(50%)</span></p>
+            </div>
+            <Link href='https://curve.fi/#/ethereum/pools/factory-tricrypto-14/deposit' rel="noopener noreferrer" target="_blank" className='px-2 text-muted'>
+              {shortenAddress('0x2570f1bd5d2735314fc102eb12fc1afe9e6e7193')}
+            </Link>
+          </div>
+          <div className='mt-3'>
+            <div className='flex gap-2 items-center justify-between'>
+              <div className='flex items-center'>
+                <Image alt='balancer' src={curve} className='inline-block align-middle mr-1' height={20} width={20} />
+                <p>TryLSD</p>
+              </div>
+              <p>512.21 ETH <span className='text-red'>(50%)</span></p>
+            </div>
+            <Link href='https://curve.fi/#/ethereum/pools/factory-tricrypto-14/deposit' rel="noopener noreferrer" target="_blank" className='px-2 text-muted'>
+              {shortenAddress('0x2570f1bd5d2735314fc102eb12fc1afe9e6e7193')}
+            </Link>
+          </div>
+        </div>
+      </div>
+      {/* ----------------------- MaxETH pool composition----------------------- */}
+      <div>
+        <h2 className='text-secondary text-xl'>MaxETH Composition</h2>
+        <div className='flex justify-around'>
+          <div className='shrink-0'>
+            <MaxEthChart data={curvePoolData} />
+          </div>
+          <div className='shrink-0'>
+            <MaxEthChart data={balancerPoolData} />
+          </div>
+        </div>
+      </div>
+      {/* ----------------------- MaxETH pool Description----------------------- */}
+      <div className='flex flex-col gap-1 mt-2'>
+        <h2 className='text-secondary text-xl'>MaxETH Composition</h2>
+        <p>This is a pool of pools to get extra yield on staked ETH to maximize yield on ETH</p>
+      </div>
+      {/* ----------------------- MaxETH pool fees----------------------- */}
+      <div className='flex flex-col gap-1 mt-2'>
+        <h2 className='text-secondary text-xl'>Fees</h2>
+        <div className='flex gap-3 items-center'>
+          <div>
+            <h3>Base Fee</h3>
+            <p>0.2%</p>
+          </div>
+          <div>
+            <h3>Protocol Fee</h3>
+            <p>5%</p>
+          </div>
+        </div>
+      </div>
     </div>
     
   )
--- components/blockchain/TransactionForm.tsx
+++ components/blockchain/TransactionForm.tsx
@@ -1,18 +1,159 @@
 'use client'
 import { useState } from 'react'
 
+// UI Components
+import { Input } from '@/components/ui/input'
+import { Button } from '@/components/ui/button'
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip"
+
+
+// icons
+import { ArrowDown, Settings, Loader2, Info } from 'lucide-react'
 const TransactionForm = () => {
 
   const [ transferState, setTransferState ] = useState<string>('deposit')
+  const [amount, setAmount] = useState<string>('')
+
+
+  const handleTabChange: React.MouseEventHandler<HTMLButtonElement> = (event) => {
+    console.log('transferState ->', transferState)
+    setTransferState(event.currentTarget.dataset.value as any)
+  }
+
+  const onApproveSpending = () => {
+    if (transferState === 'deposit') {
+      console.log('Approved')
+      // writeApproveETH?.()
+    }
+  }
+
 
   return (
     <form
-      className='border-2 border-border m-3 p-4 mx-auto md:w-full lg:w-1/2'
+      className='border-2 border-border rounded-xl m-3 p-4 mx-auto md:w-full'
       onSubmit={(e) => {
         e.preventDefault()
       }}
     >
-      <h1>Deposit/Withdraw</h1>
+      <div className='pb-3'>
+        <Tabs defaultValue='deposit' className='w-full lg:w-4/5 my-3 mx-auto'>
+          <TabsList className='w-full'>
+            <TabsTrigger className='w-full' onClick={handleTabChange} value="deposit" data-value="deposit">Deposit</TabsTrigger>
+            <TabsTrigger className='w-full' onClick={handleTabChange}  value="withdraw" data-value="withdraw">Withdraw</TabsTrigger>
+            <TabsTrigger className='w-full' onClick={handleTabChange}  value="swap" data-value="swap">Swap</TabsTrigger>
+          </TabsList>
+        </Tabs>
+      </div>
+      <div className=''>
+      <div className='flex flex-col justify-between items-center mt-3 p-3 bg-card rounded-lg border border-primary'>
+        <div className='flex justify-between items-center w-full'>
+          <Input 
+            aria-label="Amount ETH" 
+            onChange={(e) => setAmount(e.target.value)}
+            placeholder="0.05"
+            className='md:placeholder:text-xl text-xl bg-card'
+            value={amount} 
+          />
+          <div className='flex items-center gap-1 border border-primary/70 rounded-xl p-2 shrink-0'>
+            {/* <Image alt='icon' src={props.tokens[0].icon} className='inline-block align-middle mr-1' height={20} width={20} /> */}
+            <label className='px-1' htmlFor="amount">ETH</label>
+          </div>
+
+          </div>
+          <div className='flex justify-between items-center w-full mt-2'>
+            <p>23.23432</p>
+            {/* <p className='text-sm'>{!ethBalance.data?.value ? 
+              '--'
+              : 
+              formatToNormalizedAmount(ethBalance.data?.value)}
+            </p> */}
+            {/* <p onClick={() => maxAmount(0)} className='underline underline-offset-2 text-primary/70 hover:cursor-pointer ml-3'>Max</p> */}
+          </div>
+        </div>
+        <div className='flex justify-center my-4'>
+          <ArrowDown  />
+        </div>
+        <div className='flex flex-col justify-between items-center mt-3 p-3 bg-card rounded-lg border border-primary'>
+          <div className='flex justify-between items-center w-full'>
+            <Input 
+              aria-label="Amount MaxETH" 
+              onChange={(e) => setAmount(e.target.value)}
+              placeholder="0.05"
+              className='md:placeholder:text-xl text-xl bg-card'
+              value={amount} 
+            />
+            <div className='flex items-center gap-1 border border-primary/70 rounded-xl p-2 shrink-0'>
+              {/* <Image alt='icon' src={props.tokens[0].icon} className='inline-block align-middle mr-1' height={20} width={20} /> */}
+              <label className='px-1' htmlFor="amount">MaxETH</label>
+            </div>
+          </div>
+          <div className='flex justify-between items-center w-full mt-2'>
+            <p>0</p>
+            {/* <p className='text-sm'>{!ethBalance.data?.value ? 
+              '--'
+              : 
+              formatToNormalizedAmount(ethBalance.data?.value)}
+            </p> */}
+            {/* <p onClick={() => maxAmount(0)} className='underline underline-offset-2 text-primary/70 hover:cursor-pointer ml-3'>Max</p> */}
+          </div>
+        </div>
+        <div className='flex justify-between items-center my-3 text-sm'>
+          <div className='flex'>
+            <p>Transaction fees</p>
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger><Info className='h-4 w-4 ml-2' /></TooltipTrigger>
+                <TooltipContent>
+                  <div className='mb-2'>
+                    <h6 className='text-primary'>Gas Fee</h6>
+                    <p className='ml-2 text-sm text-muted'>0.0000123</p>
+                  </div>
+                  <div className='mb-2'>
+                    <h6 className='text-primary'>Base Fee</h6>
+                    <p className='ml-2 text-sm text-muted'>0.0000123</p>
+                  </div>
+                  <div className='mb-2'>
+                    <h6 className='text-primary'>Slippage</h6>
+                    <p className='ml-2 text-sm text-muted'>0.0000123</p>
+                  </div>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          </div>
+
+          <p>0.0000123</p>
+          {/* <p>{gasFeeData.data?.gasPrice ? formatEther(gasFeeData.data?.gasPrice) : '0'}</p> */}
+        </div>
+        <div className='flex justify-between items-center my-3 text-sm'>
+          <p>Fee settings</p>
+          <Settings />
+          {/* <p>{gasFeeData.data?.gasPrice ? formatEther(gasFeeData.data?.gasPrice) : '0'}</p> */}
+        </div>
+      </div>
+      <div className='flex flex-col items-center justify-center w-full'>
+        <Button 
+          className='my-2 text-xl font-bold w-full' 
+          // disabled={loadingETH  || !amount.some(value => Number(value) > 0) || true}
+          onClick={() => onApproveSpending()}
+          >
+            Approve
+          {/* {isLoading ? <Loader2 className="h-4 w-4 animate-spin" />  : 'Approve'} */}
+        </Button>
+        <Button 
+          className='my-2 text-xl font-bold w-full' 
+          // disabled={loadingETH  || !amount.some(value => Number(value) > 0) || true}
+          onClick={() => onApproveSpending()}
+          >
+            Deposit
+          {/* {isLoading ? <Loader2 className="h-4 w-4 animate-spin" />  : 'Approve'} */}
+        </Button>
+      </div>
     </form>
   )
 }
