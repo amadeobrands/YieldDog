71ec4d23377e1e5caabf070b8d61fe5087a62950
Shawn Fladager <shawnfladager@gmail.com>
2023-09-12 00:44:51 +0800

    Updates deposit/withdraw working with forked mainnet local node.

--- components/blockchain/Deposit.tsx
+++ components/blockchain/Deposit.tsx
@@ -1,32 +1,83 @@
-
+import { useEffect } from 'react'
 // UI components
 import { Button } from '@/components/ui/button'
 
 // Web3 Hooks
-import { usePrepareContractWrite, useContractWrite} from 'wagmi'
+import { usePrepareContractWrite, useContractWrite, useWaitForTransaction, useContractRead } from 'wagmi'
 
-export const Deposit = (amountToTransfer: any) => {
+import { Loader2 } from 'lucide-react'
 
-  // const swapContract = {
-  //   address: 
-  //   abi:
-  // }
+// Abi's
+import gatewayAbi from '../../abi/Gateway.json'
+import masterPoolAbi from '../../abi/masterPool.json'
+
+interface DepositProps {
+  amountToTransfer: string | number | bigint | undefined;  // or the appropriate type
+  account: string | undefined;           // assuming accountAddress is a string
+}
+
+export const Deposit = ({ amountToTransfer, account }: DepositProps) => {
+
+  const gatewayContract = {
+    address: '0xde79380FBd39e08150adAA5C6c9dE3146f53029e',
+    abi: gatewayAbi.abi as any
+  } as const
+
+  const masterPoolContract = {
+    address: '0x04f1A5b9BD82a5020C49975ceAd160E98d8B77Af',
+    abi: masterPoolAbi.abi as any
+  } as const
+
+  // Gateway pool read
+  const { data: gatewaycrvLSD } = useContractRead({
+    address: gatewayContract.address,
+    abi: gatewayContract.abi,
+    functionName: 'ydLSD',
+  })
+
+  // Masterpool read balance
+  const { data: masterPoolBalance, error: masterPoolError } = useContractRead({
+    address: masterPoolContract.address,
+    abi: masterPoolContract.abi,
+    functionName: 'balanceOf',
+    args: [account ? account : '']
+  })
+
+  const { config: configDeposit, error: errorDeposit } = usePrepareContractWrite({
+    address: gatewayContract.address,
+    abi: gatewayContract.abi,
+    functionName: 'deposit',
+    value: amountToTransfer ? BigInt(amountToTransfer/6) : undefined,
+  })
+  const { data: dataDeposit, write: writeDeposit } = useContractWrite(configDeposit)
+
+  const { data: waitForTransactionData, isLoading } = useWaitForTransaction({
+    hash: dataDeposit?.hash,
+  })
 
   const onDeposit = () => {
-      console.log('Approved')
-      console.log('amountToTransfer ->', amountToTransfer)
+      // console.log('amountToTransfer ->', BigInt(amountToTransfer))
+      writeDeposit?.()
   }
 
+  useEffect(() => {
+    // console.log('data ->', dataDeposit)
+    console.log('accountAddress ->', account)
+    console.log('errorDeposit ->', errorDeposit)
+    console.log('dataDeposit ->', dataDeposit)
+    console.log('dataDeposit ->', waitForTransactionData)
+    console.log('masterPoolBalance ->', masterPoolBalance)
+    console.log('masterPoolError ->', masterPoolError)
+  },[errorDeposit, account, dataDeposit, masterPoolBalance, waitForTransactionData, masterPoolError])
 
   return (
     <div className='flex flex-col items-center justify-center w-full'>
       <Button 
         className='my-2 text-xl font-bold w-full' 
-        // disabled={loadingETH  || !amount.some(value => Number(value) > 0) || true}
+        disabled={!writeDeposit || !amountToTransfer}
         onClick={() => onDeposit()}
         >
-          Deposit
-        {/* {isLoading ? <Loader2 className="h-4 w-4 animate-spin" />  : 'Approve'} */}
+        {isLoading ? <Loader2 className="h-4 w-4 animate-spin" />  : 'Deposit'}
       </Button>
     </div>
   )
--- components/blockchain/Withdraw.tsx
+++ components/blockchain/Withdraw.tsx
@@ -1,27 +1,80 @@
 
+import { useEffect } from 'react'
 // UI components
 import { Button } from '@/components/ui/button'
+import { Loader2 } from 'lucide-react'
 
 // Web3 Hooks
-import { usePrepareContractWrite, useContractWrite} from 'wagmi'
+import { usePrepareContractWrite, useContractWrite, useContractRead, useWaitForTransaction} from 'wagmi'
 
-export const Withdraw = () => {
+// Abi's
+import gatewayAbi from '../../abi/Gateway.json'
+import masterPoolAbi from '../../abi/masterPool.json'
+
+interface DepositProps {
+  amountToTransfer: string | number | bigint | undefined;  // or the appropriate type
+  account: string | undefined;           // assuming accountAddress is a string
+}
+
+export const Withdraw = ({ amountToTransfer, account }: DepositProps) => {
+
+  const gatewayContract = {
+    address: '0xde79380FBd39e08150adAA5C6c9dE3146f53029e',
+    abi: gatewayAbi.abi as any
+  } as const
+
+  const masterPoolContract = {
+    address: '0x04f1A5b9BD82a5020C49975ceAd160E98d8B77Af',
+    abi: masterPoolAbi.abi as any
+  } as const
 
 // Approve function for ETH
-// const { config: approveETH, error: approveError } = usePrepareContractWrite({
-//   address: WSTETH_ADDRESS,
-//   abi: wstEthAbi,
-//   functionName: 'approve',
-//   args: [POOL_ADDRESS, amount],
-//   chainId: 1,
-// })
-// const { write: writeApproveWSTETH, isLoading: loadingWstETH } = useContractWrite(approveWstETH)
+const { config: approveSpend, error: approveSpendError } = usePrepareContractWrite({
+  address: masterPoolContract.address,
+  abi: masterPoolContract.abi,
+  functionName: 'approve',
+  args: [gatewayContract.address, amountToTransfer ? amountToTransfer : ''],
+  chainId: 1,
+})
+const { data: approveSpendData, write: writeApproveSpend, isLoading: loadingSpend } = useContractWrite(approveSpend)
 
+// Allowance
+const { data: masterPoolAllowance, error: masterPoolAllowanceError } = useContractRead({
+  address: masterPoolContract.address,
+  abi: masterPoolContract.abi,
+  functionName: 'allowance',
+  args: [account, gatewayContract.address]
+})
+
+const { config: configWithdraw, error: errorWithdraw } = usePrepareContractWrite({
+  address: gatewayContract.address,
+  abi: gatewayContract.abi,
+  functionName: 'redeem',
+  args: [amountToTransfer],
+})
+const { data: dataWithdraw, write: writeWithdraw, isLoading } = useContractWrite(configWithdraw)
+
+// const { data: waitForTransactionData, isLoading } = useWaitForTransaction({
+//   hash: dataDeposit?.hash,
+// })
 
   const onApproveSpending = () => {
       console.log('Approved')
+      writeApproveSpend?.()
   }
 
+  const onWithdraw = () => {
+    console.log('Withdraw')
+    writeWithdraw?.()
+  }
+
+  useEffect(() => {
+    console.log('amountToTransfer', amountToTransfer)
+    console.log('errorWithdraw', errorWithdraw)
+    console.log('dataWithdraw', dataWithdraw)
+    console.log('masterPoolAllowance', masterPoolAllowance)
+    console.log('masterPoolAllowanceError', masterPoolAllowanceError)
+  }, [amountToTransfer, errorWithdraw, dataWithdraw, masterPoolAllowance, masterPoolAllowanceError])
 
   return (
     <div className='flex flex-col items-center justify-center w-full'>
@@ -35,11 +88,10 @@ export const Withdraw = () => {
         </Button>
         <Button 
           className='my-2 text-xl font-bold w-full' 
-          // disabled={loadingETH  || !amount.some(value => Number(value) > 0) || true}
-          // onClick={() => onApproveSpending()}
+          disabled={!masterPoolAllowance}
+          onClick={() => onWithdraw()}
           >
-            Withdraw
-          {/* {isLoading ? <Loader2 className="h-4 w-4 animate-spin" />  : 'Approve'} */}
+          {isLoading ? <Loader2 className="h-4 w-4 animate-spin" />  : 'Withdraw'}
         </Button>
       </div>
   )
